a3d628d4d7ea899a2ba93d65fc2b1d6431f983c5
Jamie Williams <jwilliams492@gmail.com>
2018-01-29 21:59:39 -0800

    added debugs

--- lab-jamie/lib/storage.js
+++ lab-jamie/lib/storage.js
@@ -2,23 +2,31 @@
 
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'), {suffix: 'Prom'});
+const debug = require('debug')('http:server');
 
 const storage = module.exports = {};
 
 storage.create = (schema, item) => {
+  debug('Something was created');
+
   let json = JSON.stringify(item);
   return fs.writeFileProm(`${__dirname}/../data/${schema}/${item._id}.json`, json)
     .then(() => item);
 };
 
 storage.fetchOne = (schema, itemId) =>
-  fs.readFileProm(`${__dirname}/../data/${schema}/${itemId}.json`);
+  debug('Something was fetched');
+fs.readFileProm(`${__dirname}/../data/${schema}/${itemId}.json`);
 
 storage.fetchAll = (schema) => {
+  debug('Everything was fetched');
 };
 
 storage.update = (schema, itemId, item) => {
+  debug('Something was updated');
 };
 
 storage.destroy = (schema, itemId) => {
+  debug('Something was destroyed');
+
 };
