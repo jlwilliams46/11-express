2ad33929f24142ae475990089d9e833e29f6c60a
Jamie Williams <jwilliams492@gmail.com>
2018-01-29 21:52:59 -0800

    added debugs

--- lab-jamie/lib/server.js
+++ lab-jamie/lib/server.js
@@ -3,11 +3,13 @@
 // Application dependencies
 const express = require('express');
 const errorHandler = require('./error-handler');
+const debug = require('debug')('http');
 
 
 // Application setup
 const app = express();
 const router = express.Router();
+
 app.use('/api/v1', router);
 // app.use(bodyParser) // Applies the package to every route in the app
 
@@ -21,12 +23,14 @@ const server = module.exports = {};
 server.isOn = false;
 
 server.start = function(port, callback) {
+  debug('Server Stop');
   if(server.isOn) return callback(new Error('Server running. Cannot start server again.'));
   server.isOn = true;
   return app.listen(port, callback);
 };
 
 server.stop = function(callback) {
+  debug('Server Start');
   if(!server.isOn) return callback(new Error('Server not running. You\'re dumb. Don\'t do that.'));
   server.isOn = false;
   return app.close(callback);
